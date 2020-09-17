f5cb2b5feb0de3ca625a51629822a75ff88f6eee
Arseniy Sleptsov <arseniy.sleptsov@gmail.com>
2020-09-17 10:19:08 +0300

    finished tasks for Level 1

--- loops_in_dict.py
+++ loops_in_dict.py
@@ -84,39 +84,68 @@ for school_class in school_students:
 # Самое частое имя в классе 2: Маша
 
 
-# # Задание 4
-# # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
-# school = [
-#   {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-#   {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-# ]
-# is_male = {
-#   'Маша': False,
-#   'Оля': False,
-#   'Олег': True,
-#   'Миша': True,
-# }
-# # ???
+# Задание 4
+# Для каждого класса нужно вывести количество девочек и мальчиков в нём.
+school = [
+  {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+  {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+]
+is_male = {
+  'Маша': False,
+  'Оля': False,
+  'Олег': True,
+  'Миша': True,
+}
 
-# # Пример вывода:
-# # В классе 2a 2 девочки и 0 мальчика.
-# # В классе 3c 0 девочки и 2 мальчика.
+for school_class in school:
+    female = 0
+    male = 0
+    for name in school_class['students']:
+        if is_male[name['first_name']]:
+            male += 1
+        else:
+            female += 1
+    # school_class['female'] = female
+    # school_class['male'] = male
+    class_name = school_class['class']
+    print(f'В классе {class_name} {female} девочки и {male} мальчика.')
 
 
-# # Задание 5
-# # По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
-# school = [
-#   {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-#   {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-# ]
-# is_male = {
-#   'Маша': False,
-#   'Оля': False,
-#   'Олег': True,
-#   'Миша': True,
-# }
-# # ???
+# Пример вывода:
+# В классе 2a 2 девочки и 0 мальчика.
+# В классе 3c 0 девочки и 2 мальчика.
 
-# # Пример вывода:
-# # Больше всего мальчиков в классе 3c
-# # Больше всего девочек в классе 2a
+
+# Задание 5
+# По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
+school = [
+  {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+  {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+]
+is_male = {
+  'Маша': False,
+  'Оля': False,
+  'Олег': True,
+  'Миша': True,
+}
+for school_class in school:
+    female = 0
+    male = 0
+    for name in school_class['students']:
+        if is_male[name['first_name']]:
+            male += 1
+        else:
+            female += 1
+    # school_class['female'] = female
+    # school_class['male'] = male
+    if female > male:
+        f_class = school_class['class']
+    else:
+        m_class = school_class['class']
+print(f'Больше всего мальчиков в классе {m_class}')
+print(f'Больше всего девочек в классе {f_class}')
+
+
+# Пример вывода:
+# Больше всего мальчиков в классе 3c
+# Больше всего девочек в классе 2a
